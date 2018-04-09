b4b5fc39aa3953704ab40a7b3ee8e700a97f05b8
Pedro Garcia Rodriguez <pedgarrod@gmail.com>
2018-04-09 14:51:55 +0200

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,23 @@
+output "es_endpoint" {
+  value = "${aws_elasticsearch_domain.es.endpoint}"
+}
+
+output "es_arn" {
+  value ="${aws_elasticsearch_domain.es.arn}"
+}
+
+output "es_domain_id" {
+  value =  "${aws_elasticsearch_domain.es.domain_id}"
+}
+
+output "es_kibana_endpoint" {
+  value =  "${aws_elasticsearch_domain.es.kibana_endpoint}"
+}
+
+output "es_availability_zones_ids" {
+  value = "${aws_elasticsearch_domain.es.vpc_options.0.availability_zones}"
+}
+
+output "es_vpc_ids" {
+  value = "${aws_elasticsearch_domain.es.vpc_options.0.vpc_id}"
+}
