b75b5b2f242cd0a146b6b7e1f59c5de501db299c
Pedro Garcia Rodriguez <pedgarrod@gmail.com>
2018-04-09 15:25:25 +0200

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -5,15 +5,15 @@ variable "name" {
   type        = "string"
 }
 
+variable "subnet_ids" {
+  description = "List of VPC Subnet IDs for the Elastic Search Service EndPoints will be created."
+  type = "list"
+}
+
 variable "vpc_id" {
   default     = ""
   description = "Vpc id where the Elastic Search Service cluster will be launched."
   type        = "string"
-}
-
-variable "subnet_ids" {
-  description = "List of VPC Subnet IDs for the Elastic Search Service EndPoints will be created."
-  type = "list"
 }
 
 variable "zone_id" {
@@ -23,21 +23,9 @@ variable "zone_id" {
 }
 
 // Optional
-variable "elasticsearch_version" {
-  default     = "5.5"
-  description = "Elastic Search Service cluster version number."
-  type        = "string"
-}
-
-variable "itype" {
-  default     = "m4.large.elasticsearch"
-  description = "Elastic Search Service cluster Ec2 instance type."
-  type        = "string"
-}
-
-variable "icount" {
-  default     = 1
-  description = "Elastic Search Service cluster Ec2 instance number."
+variable "access_policies" {
+  default     = ""
+  description = "IAM policy document specifying the access policies for the domain."
   type        = "string"
 }
 
@@ -47,39 +35,27 @@ variable "dedicated_master" {
   type        = "string"
 }
 
-variable "mtype" {
+variable "encryption_enabled" {
+  default     = "no"
+  description = "Enable encription in Elastic Search."
+  type        = "string"
+}
+
+variable "encryption_kms_key_id" {
   default     = ""
-  description = "Elastic Search Service cluster dedicated master Ec2 instance type."
+  description = "Enable encription in Elastic Search."
   type        = "string"
 }
 
-variable "mcount" {
-  default     = 0
-  description = "Elastic Search Service cluster dedicated master Ec2 instance number."
+variable "elasticsearch_version" {
+  default     = "5.5"
+  description = "Elastic Search Service cluster version number."
   type        = "string"
 }
 
-variable "zone_awareness" {
-  default     = false
-  description = "Indicates whether zone awareness is enabled."
-  type        = "string"
-}
-
-variable "ingress_allow_security_groups" {
-  default     = []
-  description = "Specifies the ingress security groups allowed."
-  type        = "list"
-}
-
-variable "ingress_allow_cidr_blocks" {
-  default     = []
-  description = "Specifies the ingress CIDR blocks allowed."
-  type        = "list"
-}
-
-variable "rest_action_multi_allow_explicit_index" {
-  default     = "true"
-  description = "Specifies whether explicit references to indices are allowed inside the body of HTTP requests."
+variable "icount" {
+  default     = 1
+  description = "Elastic Search Service cluster Ec2 instance number."
   type        = "string"
 }
 
@@ -92,6 +68,48 @@ variable "indices_fielddata_cache_size" {
 variable "indices_query_bool_max_clause_count" {
   default     = 1024
   description = "Maximum number of clauses allowed in a Lucene boolean query."
+  type        = "string"
+}
+
+variable "ingress_allow_cidr_blocks" {
+  default     = []
+  description = "Specifies the ingress CIDR blocks allowed."
+  type        = "list"
+}
+
+variable "ingress_allow_security_groups" {
+  default     = []
+  description = "Specifies the ingress security groups allowed."
+  type        = "list"
+}
+
+variable "itype" {
+  default     = "m4.large.elasticsearch"
+  description = "Elastic Search Service cluster Ec2 instance type."
+  type        = "string"
+}
+
+variable "mcount" {
+  default     = 0
+  description = "Elastic Search Service cluster dedicated master Ec2 instance number."
+  type        = "string"
+}
+
+variable "mtype" {
+  default     = ""
+  description = "Elastic Search Service cluster dedicated master Ec2 instance type."
+  type        = "string"
+}
+
+variable "zone_awareness" {
+  default     = false
+  description = "Indicates whether zone awareness is enabled."
+  type        = "string"
+}
+
+variable "rest_action_multi_allow_explicit_index" {
+  default     = "true"
+  description = "Specifies whether explicit references to indices are allowed inside the body of HTTP requests."
   type        = "string"
 }
 
@@ -110,11 +128,5 @@ variable "volume_size" {
 variable "volume_type" {
   default     = "gp2"
   description = "Default type of the EBS volumes."
-  type        = "string"
-}
-
-variable "access_policies" {
-  default     = ""
-  description = "IAM policy document specifying the access policies for the domain."
   type        = "string"
 }
