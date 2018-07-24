1cf176c58d583e4488ec3d834458080f0fb0f25b
Pau Rosello <paurosello@gmail.com>
2018-07-24 14:56:09 +0200

    Formatting

--- main.tf
+++ main.tf
@@ -18,11 +18,11 @@ resource "aws_security_group_rule" "secure_cidrs" {
 resource "aws_security_group_rule" "secure_sgs" {
   count = "${length(split(",", var.ingress_allow_security_groups))}"
 
-  type            = "ingress"
-  from_port       = 443
-  to_port         = 443
-  protocol        = "tcp"
-  source_security_group_id  = "${element(split(",",var.ingress_allow_security_groups), count.index)}"
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  source_security_group_id = "${element(split(",",var.ingress_allow_security_groups), count.index)}"
 
   security_group_id = "${aws_security_group.elasticsearch.id}"
 }
@@ -40,15 +40,14 @@ resource "aws_security_group_rule" "nonsecure_cidrs" {
 resource "aws_security_group_rule" "nonsecure_sgs" {
   count = "${length(split(",", var.ingress_allow_security_groups))}"
 
-  type            = "ingress"
-  from_port       = 80
-  to_port         = 80
-  protocol        = "tcp"
-  source_security_group_id  = "${element(split(",",var.ingress_allow_security_groups), count.index)}"
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  source_security_group_id = "${element(split(",",var.ingress_allow_security_groups), count.index)}"
 
   security_group_id = "${aws_security_group.elasticsearch.id}"
 }
-
 
 resource "aws_security_group_rule" "egress_all" {
   type        = "egress"
@@ -63,6 +62,7 @@ resource "aws_security_group_rule" "egress_all" {
 resource "aws_elasticsearch_domain" "es" {
   domain_name           = "${var.name}"
   elasticsearch_version = "${var.elasticsearch_version}"
+
   encrypt_at_rest {
     enabled    = "${var.encryption_enabled}"
     kms_key_id = "${var.encryption_kms_key_id}"
