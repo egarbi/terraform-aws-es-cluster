4ba9d7f9b15f4b66418af6efce3c35a46da048ce
Enrique Garbi <quique@enriquegarbi.com.ar>
2020-12-17 14:14:04 +0100

    Removes Interpolation-only expressions
    Interpolation-only expressions are deprecated

--- example/main.tf
+++ example/main.tf
@@ -9,12 +9,12 @@ data "aws_vpc" "default" {
 }
 
 data "aws_subnet_ids" "default" {
-  vpc_id = "${data.aws_vpc.default.id}"
+  vpc_id = data.aws_vpc.default.id
 }
 
 data "aws_subnet" "default" {
-  count = "${length(data.aws_subnet_ids.default.ids)}"
-  id    = "${tolist(data.aws_subnet_ids.default.ids)[count.index]}"
+  count = length(data.aws_subnet_ids.default.ids)
+  id    = tolist(data.aws_subnet_ids.default.ids)[count.index]
 }
 
 data "aws_route53_zone" "selected" {
@@ -29,13 +29,13 @@ module "es-cluster" {
   source = "../"
 
   name                      = "example"
-  vpc_id                    = "${data.aws_vpc.default.id}"
-  subnet_ids                = ["${data.aws_subnet.default.0.id}", "${data.aws_subnet.default.1.id}"]
-  zone_id                   = "${data.aws_route53_zone.selected.zone_id}"
+  vpc_id                    = data.aws_vpc.default.id
+  subnet_ids                = [data.aws_subnet.default.0.id, data.aws_subnet.default.1.id]
+  zone_id                   = data.aws_route53_zone.selected.zone_id
   itype                     = "m4.large.elasticsearch"
   icount                    = 2
   zone_awareness            = true
-  ingress_allow_cidr_blocks = ["${data.aws_vpc.default.cidr_block}"]
+  ingress_allow_cidr_blocks = [data.aws_vpc.default.cidr_block]
   access_policies           = <<CONFIG
 {   
     "Version": "2012-10-17",
